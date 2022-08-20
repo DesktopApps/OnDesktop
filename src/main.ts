beed44cc87eb539cc99c586b89d00d77bb2f33aa
gemsvido <gemsvido@gmail.com>
2022-08-20 15:49:54 -0700

    good good soonTM

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,43 @@
-import {app, BrowserWindow} from "electron";
+import {app, BrowserWindow, BrowserWindowConstructorOptions, nativeImage} from "electron";
 import path from "path";
 import url from "url";
 
+const options : BrowserWindowConstructorOptions = {
+  title: "OnDesktop",
+  icon: nativeImage.createFromPath(path.join(__dirname, 'icon.png')),
+  darkTheme: true,
+  show: false,
+  width: 800,
+  height: 600,
+  webPreferences: {
+    devTools: false,
+    javascript: true,
+    contextIsolation: true,
+    autoplayPolicy: "no-user-gesture-required",
+    spellcheck: false,
+    enableWebSQL: false,
+    sandbox: true,
+  },
+}
+
+const window : BrowserWindow = new BrowserWindow(options);
+
+
 function createWindow () {
 
-  // Create the browser window.
-  let window = new BrowserWindow({width: 800, height: 600})
+  
 
-  // and load the index.html of the app.
+
+
+  // Load the index.html of the app.
   window.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }));
-  
+
+
+  window.show();
 }
 
 app.on('ready', createWindow);
